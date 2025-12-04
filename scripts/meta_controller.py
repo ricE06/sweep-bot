2b4942ed8ac1fdad5acd4e3d06216b6273b2e012
ricE06 <ezzhaneri06@gmail.com>
2025-12-04 06:39:50 -0500

    minor updates to instructions

--- scripts/meta_controller.py
+++ scripts/meta_controller.py
@@ -10,12 +10,20 @@ from pydrake.all import (
 # Metacontroller oversees a several step process:
 # First, it outputs positions from the trajectory opt.
 # Then, it outputs positions from a TODO defined function that takes in
-# the pregrip pose and the grip pose, runs a linear interpolant, outputs that
+# the pregrip pose and the grip pose, runs a make_interpolant_trajectory, outputs that
 # Then, it outputs positions from a TODO defined function that takes in
 # a bunch of broom base poses and returns grip poses.
 # When using the broom grasp calculator, set the angle to 0 for now.
 # You likely need a specialized "switch" input port to feed signals into
 # in order to determine which of these phases we are in.
+
+# The make_interpolant_trajectory will take in keyframes (similar to pset) 
+# of desired positions + the width of the gripper. It will go through them
+# one by one for some (configurable) time frame. 
+# If any of you figure out how to dynamically string together trajectory sources,
+# that is amazing and we should do that, but my intuition leads me to
+# writing a custom controller for this
+# Essentially just a giant switch case statement
 
 # (after mtg)
 # turn on position + torque control. ideally we'd be in full torque control
