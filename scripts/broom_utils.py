09b50f6cec859b9abda9e4a8ca6fb0ad87305b20
ricE06 <ezzhaneri06@gmail.com>
2025-12-07 04:57:32 -0500

    progress on getting broom grasp locations

--- scripts/broom_utils.py
+++ scripts/broom_utils.py
@@ -12,6 +12,8 @@ def angle_between_vectors(v1: np.ndarray, v2: np.ndarray):
 X_Joint = RigidTransform(RollPitchYaw(-0.5236, 0, 0), np.array([0, 0.2, 0.35]))
 X_handle_offset = RigidTransform(RollPitchYaw(0, 0, 0), np.array([0, 0, 0.2]))
 
+X_offset = X_Joint @ X_handle_offset
+
 GRIP_DIST = 0.01
 PREGRIP_DIST = 0.2
 
@@ -19,31 +21,27 @@ PREGRIP_DIST = 0.2
 
 # currently broken, should try to point to center of robot
 def compute_broom_grasp_angle(broom_base: RigidTransform, robot_center: np.ndarray):
-    X_Whandle: RigidTransform = broom_base @ X_Joint @ X_handle_offset
+    X_Whandle: RigidTransform = broom_base @ RigidTransform(RotationMatrix(), X_offset.translation())
     handle_pos = X_Whandle.translation()
-
-    # compute amount of rotation need to align to robot center
-    y_vec = X_Whandle.rotation().matrix() @ np.array([0, 1, 0])
-    z_vec = X_Whandle.rotation().matrix() @ np.array([0, 0, 1])
     robot_to_handle = handle_pos - robot_center
-    print(robot_to_handle)
-    robot_to_handle_proj = robot_to_handle - (np.dot(robot_to_handle, z_vec) * z_vec / np.linalg.norm(z_vec))
-    angle = angle_between_vectors(y_vec, robot_to_handle_proj)
-
-    # do we turn left or right? need dot prod with orthogonal y to know
-    # sign_bool = angle_between_vectors(np.array([-y_vec[1], y_vec[0]]), robot_to_handle[0:2]) >= 0
-    # sign = 1 if sign_bool else -1
-    # angle = -sign * angle
-    print(angle)
+    angle = np.atan2(robot_to_handle[1], robot_to_handle[0]) - np.pi/2
     return angle
 
+def compute_wrist_rotation_angle(cur_grasp: RigidTransform): 
+    # project out component in direction of the gripper
+    gripper_vec = cur_grasp.rotation().matrix()[:, 1]
+    print(gripper_vec)
+
+def _get_broom_offset(broom_base: RigidTransform, angle: float, offset: float):
+    """
+    Computes a grip for the broom at broom_base (on the handle), pointing in angle,
+    with offset (positive radially outward at this angle)
+    """
+    broom_transposed = broom_base @ RigidTransform(RollPitchYaw(0, 0, angle), X_offset.translation())
+    return broom_transposed
 
 def get_broom_grip(broom_base: RigidTransform, angle: float):
-    X_Whandle: RigidTransform = broom_base @ X_Joint @ X_handle_offset
-    X_Whandle_rotated = X_Whandle @ RigidTransform(RollPitchYaw(0, 0, -angle), np.array([0, 0, 0])) 
-    X_out =  X_Whandle_rotated @ RigidTransform(RollPitchYaw(0, np.pi, 0), np.array([0, -GRIP_DIST, 0]))
-
-    return X_out
+    return _get_broom_offset(broom_base, angle, GRIP_DIST)
 
 def get_broom_pregrip(broom_base: RigidTransform, angle: float):
     # move to the handle location, but don't rotate
